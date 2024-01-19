2b19f578546429f859463db8e56a44948c66485f
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2024-01-19 12:08:28 -0700

    build a header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 import images from '../constants/images'
-import Container from './Container'
 import HeadingImage from './HeadingImage'
 import './Header.css'
 import SubHeader from './SubHeader'
 
 const Header = ({}) => {
   return (
-    <div className="header">
-     <Container>
-        <SubHeader image={images.spoon} />
-        <HeadingImage image={images.welcome} />
-     </Container>
+    <div className="header section" id='header'>
+     <div className='container'>
+        <div className="header__grid">
+          <SubHeader image={images.spoon} />
+          <HeadingImage image={images.welcome} />
+        </div>
+     </div>
     </div>
   )
 }
