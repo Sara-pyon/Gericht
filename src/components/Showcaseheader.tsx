b06ebe68f19cceac5bd17379db2688e2883b1f64
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2024-01-22 11:45:09 -0700

    build a shoucase heading component

---
+++ src/components/Showcaseheader.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TitleWSpoon from './TitleWSpoon'
+
+interface Props{
+    subTitle: string;
+    title: string;
+}
+
+const Showcaseheader = ({subTitle, title}: Props) => {
+  return (
+    <div className="showcase__header">
+        <TitleWSpoon title={subTitle} />
+        <h2 className="section-heading">{title}</h2>
+    </div>
+  )
+}
+
+export default Showcaseheader
